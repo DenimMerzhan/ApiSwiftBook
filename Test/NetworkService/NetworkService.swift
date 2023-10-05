6af0e554537c8c38e6b4322c1b5c463764e72a3c
Denim <dagn456@mail.ru>
2023-10-05 17:10:19 +0300

    NetworkService configured

---
+++ Test/NetworkService/NetworkService.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkService.swift
+//
+//
+//  Created by Деним Мержан on 05.10.23.
+//
+
+import Foundation
+
+
+class NetworkService {
+    
+    static let shared = NetworkService()
+    
+    func fetchAvatar(from url: URL, completion: @escaping (Data) -> Void) {
+        
+        DispatchQueue.global(qos: .default).async {
+            
+            guard let imageData = try? Data(contentsOf: url) else {return}
+            
+            DispatchQueue.main.async {
+                completion(imageData)
+            }
+
+        }
+    }
+    
+    private init() {}
+    
+}
+
